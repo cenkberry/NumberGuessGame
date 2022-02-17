b83ba1e77142634a2d84068fb000fe17454167fb
cenkberry <cenkberi@mail.com>
2022-02-17 16:56:15 +0300

    ready

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ let pcAnswer       = document.querySelector('.pcAnswer');
 const startGame    = document.querySelector('.startGame');
 const butU         = document.querySelector('.butU');
 const butD         = document.querySelector('.butD');
+const corBut       = document.querySelector('.corBut');
 
+butU.style.display = "none";
+butD.style.display = "none";
+corBut.style.display = "none";
 
 startGame.addEventListener("click", () => {
   pcAnswer.innerHTML = "50?";
   localStorage.setItem("min" , 0);
   localStorage.setItem("max" , 100);
   localStorage.setItem("guess", 50);
+  butU.style.display = "inline-block";
+  butD.style.display = "inline-block";
+  corBut.style.display = "inline-block";
 });
-
 
 butU.addEventListener("click", () => {
   let max   = localStorage.getItem("max");
   let guess = localStorage.getItem("guess");
-  
   let prediction = Math.floor( ( Number(max) + Number(guess) )/2);
   pcAnswer.innerHTML = prediction + "?";
-
   localStorage.setItem("guess" , prediction);
   localStorage.setItem("min" , guess);
 });
@@ -26,10 +30,18 @@ butU.addEventListener("click", () => {
 butD.addEventListener("click", () => {
   let min = localStorage.getItem("min");
   let guess = localStorage.getItem("guess");
-   
   let prediction = Math.floor( ( Number(guess) + Number(min))/2);
   pcAnswer.innerHTML = prediction + "?";
-
   localStorage.setItem("guess" , prediction);
   localStorage.setItem("max" , guess);
+});
+
+corBut.addEventListener("click", () => {
+  pcAnswer.innerHTML = " ";
+  localStorage.setItem("min" , 0);
+  localStorage.setItem("max" , 0);
+  localStorage.setItem("guess", 0);
+  butU.style.display = "none";
+  butD.style.display = "none";
+  corBut.style.display = "none";
 });
