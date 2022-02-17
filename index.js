eec028c9121a5fe5ea7b34fabac0d5e96b0ff348
cenkberry <cenkberi@mail.com>
2022-02-17 12:14:28 +0300

    building

---
+++ index.js
@@ -0,0 +1,13 @@
+const pcAnswer   = document.querySelector('.pcAnswer');
+const userNum    = document.getElementById('userNum').value;
+const startGame  = document.querySelector('.startGame');
+const butU       = document.querySelector('.butU');
+const butD       = document.querySelector('.butD');
+
+
+startGame.addEventListener("click", () => {
+
+  console.log(userNum);
+ 
+
+});
